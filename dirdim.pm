00d5f91e159f7e1b3e8f79668798c45c2a5b780f
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-04-25 21:43:09 +0900

    7個のファイルを用意した。さらにdirdimについては、オプションの文字をいくつか変更し、表頭の表示の書式も変えた。2次情報もイタリック太字フェイントにした。

---
+++ dirdim.pm
@@ -0,0 +1,32 @@
+package App::dirdim ;  
+our $VERSION = '0.030' ; 
+our $DATE = '2022-04-25T21:05+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::dirdim
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<dirdim>'. 
+
+=head1 DESCRIPTION
+
+=head1
+
+
+=head1 AUTHOR
+
+下野寿之 Toshiyuki SHIMONO <bin4tsv@gmail.com> 統計数理研究所 外来研究員
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2022 Toshiyuki SHIMONO. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1 ;
